9b2448407b17acfb8444e4b055cfa7a5afe900d9
Bukhori Muslim Saloran <bori.limzz@gmail.com>
2021-09-21 09:08:11 +0700

    inisiasi, custom views, setup auth, dan user model & entity

---
+++ app/Views/templates/sidebar.php
@@ -0,0 +1,82 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-scroll"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Uji Profisiensi BBPK</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Menu Pengguna
+    </div>
+
+
+    <!-- Nav Item - MyProfile -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-user"></i>
+            <span>Profil</span></a>
+    </li>
+
+    <!-- Nav Item - EditProfile -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-user-edit"></i>
+            <span>Ubah Profil</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Menu Uji Profisiensi
+    </div>
+
+
+    <!-- Nav Item - CalenderUP -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="far fa-calendar-alt"></i>
+            <span>Informasi Kalender Uji Profisiensi</span></a>
+    </li>
+
+    <!-- Nav Item - SignupUP -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-list-ul"></i>
+            <span>Daftar Uji Profisiensi</span></a>
+    </li>
+
+    <!-- Nav Item - Current -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-tasks"></i>
+            <span>Uji Profisiensi yang sdiikuti</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Nav Item - Log out -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Log Out</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
